c998537a8bacf33756720369588e72f0c74d4521
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-07-31 15:20:48 +0200

    feat app.js> base route

---
+++ app.js
@@ -0,0 +1,13 @@
+// importo express
+const express = require(`express`);
+
+// variabile che contiene un'istanza di express
+const app = express();
+
+// definiamo la porta d'ascolto del server
+const port = 3000;
+
+// definiamo la rotta base dell'applicazione
+app.get(`/`, (req, res) => {
+  
+})
