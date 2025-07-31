2d9fbc35beb9849ad170115fdc57ca3d1a03be4e
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-07-31 16:00:32 +0200

    feat: /bacheca route; add imgs

--- app.js
+++ app.js
@@ -17,34 +17,38 @@ const posts = [
   {
     title: "Uomo sulla Luna",
     content: "Lorem ipsum dolor",
-    img: "./",
+    img: "./public/imgs/Moon.png",
     tags: ["space","nasa","moon"]
   },
   {
     title: "Uomo su Marte",
     content: "Lorem ipsum dolor",
-    img: "./",
+    img: "./public/imgs/Mars.png",
     tags: ["space","nasa","mars"]
   },
   {
     title: "Uomo su Venere",
     content: "Lorem ipsum dolor",
-    img: "./",
+    img: "./public/imgs/Venus.png",
     tags: ["space","nasa","Venus"]
   },
   {
     title: "Uomo su mercurio",
     content: "Lorem ipsum dolor",
-    img: "./",
+    img: "./public/imgs/Mercury.png",
     tags: ["space","nasa","mercury"]
   },
   {
     title: "Uomo su Giove",
     content: "Lorem ipsum dolor",
-    img: "./",
+    img: "./public/imgs/Jupiter.png",
     tags: ["space","nasa","Jupiter"]
   },
 ]
+
+app.get(`/bacheca`, (req, res) => {
+  res.json(posts)
+})
 
 app.listen(port, () => {
   console.log(`Server del mio blog`)
