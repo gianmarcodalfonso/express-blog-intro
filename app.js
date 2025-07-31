fc296bca8de7f1cc9b993c4892e453cc301e4693
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-07-31 15:42:12 +0200

    feat: app.get; app.listen

--- app.js
+++ app.js
@@ -9,5 +9,11 @@ const port = 3000;
 
 // definiamo la rotta base dell'applicazione
 app.get(`/`, (req, res) => {
-  
+  console.log(`Server del mio blog`)
+  res.send(``)
+})
+
+
+app.listen(port, () => {
+  console.log(`Server del mio blog`)
 })
