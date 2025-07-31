0eaa6447305aab113f2eef0a089aa504c2d2b8ae
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-07-31 16:45:09 +0200

    fix: imgs-preview

--- app.js
+++ app.js
@@ -16,40 +16,41 @@ app.get(`/`, (req, res) => {
 // Inserisco il middleware per i file statici
 app.use(express.static(`public`));
 
-const posts = [
-  {
-    title: "Uomo sulla Luna",
-    content: "Lorem ipsum dolor",
-    img: "public/imgs/Moon.png",
-    tags: ["space","nasa","moon"]
-  },
-  {
-    title: "Uomo su Marte",
-    content: "Lorem ipsum dolor",
-    img: "./public/imgs/Mars.png",
-    tags: ["space","nasa","mars"]
-  },
-  {
-    title: "Uomo su Venere",
-    content: "Lorem ipsum dolor",
-    img: "./public/imgs/Venus.png",
-    tags: ["space","nasa","Venus"]
-  },
-  {
-    title: "Uomo su mercurio",
-    content: "Lorem ipsum dolor",
-    img: "./public/imgs/Mercury.png",
-    tags: ["space","nasa","mercury"]
-  },
-  {
-    title: "Uomo su Giove",
-    content: "Lorem ipsum dolor",
-    img: "./public/imgs/Jupiter.png",
-    tags: ["space","nasa","Jupiter"]
-  },
-]
 
 app.get(`/bacheca`, (req, res) => {
+  const posts = [
+    {
+      title: "Uomo sulla Luna",
+      content: "Lorem ipsum dolor",
+      img: "imgs/Moon.jpg",
+      tags: ["space","nasa","moon"]
+    },
+    {
+      title: "Uomo su Marte",
+      content: "Lorem ipsum dolor",
+      img: "imgs/Mars.png",
+      tags: ["space","nasa","mars"]
+    },
+    {
+      title: "Uomo su Venere",
+      content: "Lorem ipsum dolor",
+      img: "imgs/Venus.png",
+      tags: ["space","nasa","Venus"]
+    },
+    {
+      title: "Uomo su mercurio",
+      content: "Lorem ipsum dolor",
+      img: "imgs/Mercury.png",
+      tags: ["space","nasa","mercury"]
+    },
+    {
+      title: "Uomo su Giove",
+      content: "Lorem ipsum dolor",
+      img: "imgs/Jupiter.png",
+      tags: ["space","nasa","Jupiter"]
+    },
+  ]
+  
   res.json(posts)
 })
 
